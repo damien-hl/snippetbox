e55b21a08179c1b2ea2e839c2ecca9ba687c1411
Damien Heulin <damienheulin87@gmail.com>
2023-02-27 18:21:07 +0100

    feat: render data in home and view templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/damien-hl/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
