e74236f4af09805a33a5395d1d057c280c369ea9
Damien Heulin <damienheulin87@gmail.com>
2023-02-28 11:02:56 +0100

    feat: add log request and security headers middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,8 +2,9 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(neuteredFileSystem{fs: http.Dir("./ui/static/")})
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
@@ -11,5 +12,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return app.logRequest(secureHeaders(mux))
 }
